2ed1d28b4ecf5aa0eb088b16a8d6adb73b174b16
Dodoh04 <douglasoliversa3@gmail.com>
2022-12-01 22:18:16 -0300

    fim das listViw de filmes

--- app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
+++ app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
@@ -6,10 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ListView listaFilmes;
+    //criar base de dados para os filmes
+    String titulo[] = {"O Apocalipse","Desejos Ocultos", "2067", "Incriminada", "O rei da Montanha", "Os escolhidos"};
+    String desc [] = {"Objetos não identificados estão a caminho de se chocar contra a Terra.","Neste suspense futurista, um casal, Sophie e James compram um robô ultra realista para cuidar de sua nova casa.","A jornada de um homem para o futuro para salvar um mundo moribundo.",
+    "Annie decide usar o carro do marido durante uma viagem repentina que ele fez trabalho. Ao ser parada pela polícia.", "Espen Ash Lad, filho de um pobre agricultor, embarca em uma missão perigosa com seus irmãos para salvar a princesa de um vilão conhecido como o Rei da Montanha.",
+    "Lacey e Daniel descobrem que sua família é alvo de uma força mortal inimaginável e aterrorizante. Ambos terão que fazer tudo o que estiver ao seu alcance para resolver esse mistério sombrio e salvar suas vidas."};
+    String acesso [] = {"4.5","3.3","4.9","5.9", "2.4", "3.9"};
+    int filmes [] = {R.drawable.apocalipse,R.drawable.desejosocultod, R.drawable.num, R.drawable.icriminada, R.drawable.oreidamontanha, R.drawable.osescolhidos};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 0;
+            return filmes.length;
         }
 
         @Override
@@ -44,7 +54,28 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            return null;
+            //variaveis do modelo
+            ImageView imagemFilme;
+            TextView tituloFilme, descricaoFilme, acessoFilme;
+
+            //adaptador ligando ao modelo
+
+            View v = getLayoutInflater().inflate(R.layout.modelo_filmes, null);
+
+            //Apresentar as Variaveis do java para o modelo xml3
+
+            imagemFilme = v.findViewById(R.id.imgFilme);
+            tituloFilme = v.findViewById(R.id.tituloFilme);
+            descricaoFilme = v.findViewById(R.id.descFilme);
+            acessoFilme = v.findViewById(R.id.acesso);
+
+
+            tituloFilme.setText(titulo[i]);
+            descricaoFilme.setText(desc[i]);
+            acessoFilme.setText(acesso[i]);
+            imagemFilme.setImageResource(filmes[i]);
+
+            return v;
         }
     }
 }
